69cfb5903ee839afb76db853d3dd47913a325dc4
Jonathan Nava <jonathan.navaflores@gmail.com>
2022-10-26 12:25:04 -0500

    fix #2 added var: max_size

--- automate_catalog_information/changeImage.py
+++ automate_catalog_information/changeImage.py
@@ -2,10 +2,11 @@
 from PIL import Image
 import os
 path = 'images/'
+max_size = (600,400)
+
 images = [file for file in os.listdir(path) if file.endswith(('tiff'))]
 
 for image in images:
     file_name = image.split('.')[0]
     img = Image.open(path+image).convert('RGB')
-    img.resize((600,400)).save(path+'{}.jpeg'.format(file_name))
-  
+    img.resize(max_size).save(path+'{}.jpeg'.format(file_name))
