3b12f06d70c2fb0ed11bc37aedcfd3683f83c643
Jonathan Nava <jonathan.navaflores@gmail.com>
2022-10-26 16:33:21 -0500

    update test files

--- automate_catalog_information/run.py
+++ automate_catalog_information/run.py
@@ -2,6 +2,7 @@
 
 import requests
 import os
+import json
 
 url = "http://localhost/upload/"
 
@@ -11,13 +12,26 @@ descriptions_path = 'descriptions/'
 descriptions = os.listdir(descriptions_path)
 
 images = [file for file in os.listdir(images_path) if file.endswith(('jpeg'))]
-print(images)
-print(descriptions)
+#print(images)
+#print(descriptions)
+
+for file in descriptions:
+    with open(descriptions_path + file, 'r') as f:
+        line = f.readlines()
+        
 
 
-#for image in images:
-    #print(images)
-    
+
+
+#print(type(descriptions))
+
+#json_object = json.dumps(descriptions)
+
+#print(type(json_object))
+#print((json_object))
+
+#TODO:Write a Python script named run.py to process the text files (001.txt, 003.txt ...) from the supplier-data/descriptions directory. The script should turn the data into a JSON dictionary by adding all the required fields
+
 
 # for image in images:
 #     with open(path+image, 'rb') as upload_img:
